80750c8c55bb5ae0a75b991a801edc71f8ba06de
Aashis Rimal <aashis_rimal@yahoo.com>
2018-11-07 21:02:03 +0200

    Added product model to define the schema of product object

---
+++ models/product.js
@@ -0,0 +1,37 @@
+//import mongoose
+const mongoose = require('mongoose');
+
+//create variable for mongoose schema object
+const Schema = mongoose.Schema;
+
+//Require the mongoose-currency node module and load the new type to mongoose
+require('mongoose-currency').loadType(mongoose);
+//Now, Currency is the new data type in mongoose just like Number or String
+const Currency = mongoose.Types.Currency;
+
+//create a new schema or a blueprint for product object
+const product = new Schema({
+    imagePath: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    }
+}, {
+    //Creates the created and modified time automatically
+    timestamps: true
+});
+
+//export this model as a module to be used in other places
+module.exports = mongoose.model('Product', product);
