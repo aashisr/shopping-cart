cb637809dc968e913aaa8bf7233e7cd6bff2c2ae
Aashis Rimal <aashis_rimal@yahoo.com>
2018-11-12 22:37:04 +0200

    Added add-product page, added routes for get and post add-product

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+//Import products model and store in Products variable
+const Products = require('../models/products');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    //Find all the products from the Products model i.e products collection in database
+    Products.find()
+        .then((products) => {
+            res.render('index.ejs', {
+                products: products,
+                title: 'Shopping Cart'
+            });
+        }, (err) => next(err)) //sends the error to the error handler
+        .catch((err) => next(err));
 });
 
 module.exports = router;
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,7 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //Import products model and store in Products variable
 const Products = require('../models/products');
@@ -22,6 +23,25 @@ productRouter.route('/')
                     title: 'Shopping Cart'
                 });
             }, (err) => next(err)) //sends the error to the error handler
+            .catch((err) => next(err));
+    });
+
+productRouter.route('/add-product')
+    .get((req, res, next) => {
+        res.render('product/addProduct.ejs', {
+            title: 'Add Product'
+        });
+    })
+    .post((req, res, next) => {
+        console.log(req.body);
+        //Post the parsed request to the Products model i.e products collection
+        //req.body is already parsed by bodyParser
+        Products.create(req.body)
+            .then((product) => {
+                res.render('product/addProduct.ejs', {
+                    title: 'Add Product'
+                });
+            }, (err) => next(err))
             .catch((err) => next(err));
     });
 
